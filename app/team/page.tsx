1b4d188d2eb1629973119ec26e162d0569fe8c0a
LoggeL <hyper.xjo@gmail.com>
2024-03-03 03:17:19 +0100

    Initial Redesign
    Co-authored-by: Max <1niceEnte@users.noreply.github.com>

---
+++ app/team/page.tsx
@@ -0,0 +1,361 @@
+import { ParallaxScroll } from '@/components/ui/parallax-scroll'
+
+const team = {
+  plays: [
+    '2016 “Die Geschichte der Black Swan”',
+    '2017 “Verrat im Kloster”',
+    '2018 “Bluttribut”',
+    '2019 “Dystopia”',
+    '2020 “Der Kristall der Träume”',
+    '2021 “Malleus Maleficarum”',
+    '2022 “Goldfieber”',
+    '2023 “Traum von Freiheit”',
+  ],
+  current: [
+    {
+      name: 'Anton',
+      roles: [
+        'Sir Richard Black',
+        'Hauptmann Lorentz',
+        'Graf Harras',
+        'Alan',
+        'Kaiser Richard',
+        'Fürstbischof Reinhard',
+        'Morgan Bullock',
+        'Laurent',
+      ],
+    },
+    {
+      name: 'Carina',
+      roles: [
+        'Jacky',
+        'Prinzessin Helen',
+        'Lisa',
+        'Cobra',
+        'Kaiserin Theresa',
+        'Helene',
+        'Maggy',
+        'Marry Reed',
+      ],
+    },
+    {
+      name: 'Elena',
+      placeholderAvatar: true,
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Fernanda',
+      placeholderAvatar: true,
+      placeholderHero: true,
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Franek',
+      roles: [null, null, null, null, null, null, 'Richie', 'Matteo'],
+    },
+    {
+      name: 'Fynn',
+      roles: [null, null, null, null, null, null, 'George', 'Admiral Rodrigo'],
+    },
+    {
+      name: 'Jonas',
+      roles: [null, null, null, null, null, 'Ensemble', 'Jesse', 'Ensemble'],
+    },
+    {
+      name: 'Johannes',
+      roles: [
+        null,
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Bestatter',
+        'Ensemble',
+      ],
+    },
+    {
+      name: 'Julian',
+      roles: [
+        'Bühnentechnik',
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Hofarzt',
+        'Ensemble',
+        'Jim / John',
+        'Ensemble',
+      ],
+    },
+    {
+      name: 'Lea',
+      roles: [
+        null,
+        null,
+        null,
+        null,
+        'Riccarda',
+        'Maren',
+        'Kate O`Hara',
+        'Anne Bonny',
+      ],
+    },
+    {
+      name: 'Leon',
+      roles: [
+        null,
+        'Graf Philipp',
+        'Johann',
+        'Lizard',
+        'Jens',
+        'Georg',
+        'Ben McConnell',
+        'Ensemble',
+      ],
+    },
+    {
+      name: 'Louis',
+      roles: [
+        null,
+        null,
+        null,
+        null,
+        'Arno',
+        'Pater Heinrich',
+        'Frank McConnel',
+        'Edward Teach “Blackbeard”',
+      ],
+    },
+    {
+      name: 'Lina',
+      roles: [null, null, null, null, null, null, 'Rose', 'Ensemble'],
+    },
+    {
+      name: 'Luisa',
+      roles: [
+        null,
+        null,
+        null,
+        'Cat',
+        'Merle',
+        'Klara',
+        'Jane',
+        'Sofia Mendoza',
+      ],
+    },
+    {
+      name: 'Magdalena',
+      roles: [null, null, null, null, null, null, 'Ensemble', 'Carmen'],
+    },
+    {
+      name: 'Maria',
+      roles: [
+        null,
+        null,
+        null,
+        null,
+        'Ensemble',
+        'Ruth',
+        'Lilly',
+        'Isabella Mendoza',
+      ],
+    },
+    {
+      name: 'Max',
+      roles: [null, null, null, null, null, 'Ensemble', 'Bill', 'Ensemble'],
+    },
+    {
+      name: 'Maximilian',
+      roles: [
+        null,
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Ensemble',
+        'Zeitungsausträger',
+        'Ensemble',
+      ],
+    },
+    {
+      name: 'Nela',
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Nele',
+      roles: [null, null, null, null, null, 'Ensemble', 'Annie', 'Megan'],
+    },
+    {
+      name: 'Niko',
+      roles: [null, null, null, null, null, null, 'Will', 'Ensemble'],
+    },
+    {
+      name: 'Raphael',
+      placeholderAvatar: true,
+      placeholderHero: true,
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Sebastian',
+      roles: [
+        'Regie',
+        'Ensemble / Regie',
+        'Albert / Regie',
+        'Ensemble / Regie',
+        'Ensemble / Regie',
+        'Ensemble / Regie',
+        'Doc William Mc Allister / Regie',
+        'Capitan Javier / Regie',
+      ],
+    },
+    {
+      name: 'Sophia',
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Theresa',
+      roles: [null, null, null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Tobias',
+      roles: [
+        null,
+        null,
+        null,
+        null,
+        'Ensemble',
+        'Ensemble',
+        'Edward Smith',
+        'Ensemble',
+      ],
+    },
+    {
+      name: 'Yunus',
+      roles: [
+        null,
+        null,
+        null,
+        null,
+        null,
+        'Technik',
+        'Thomas',
+        'Jack Rackham',
+      ],
+    },
+  ],
+  former: [
+    {
+      name: 'Anna',
+      roles: ['Susan Campbell', 'Anne', 'Ida', 'Moon', 'Emily', 'Ensemble'],
+    },
+    {
+      name: 'Anna R',
+      roles: ['Alice', 'Stumpfwaldhexe', 'Hausgeist Anastasia', 'Lilly'],
+    },
+    {
+      name: 'Anika',
+      roles: ['Ensemble', 'Ensemble', 'Ensemble'],
+    },
+    {
+      name: 'Frankie',
+      roles: [null, null, null, 'Vyper'],
+    },
+    {
+      name: 'Jack',
+      roles: [
+        null,
+        null,
+        null,
+        'Cover Moon/ Ensemble',
+        'Dr. von Hofmannsthal',
+        'Pater Markus',
+      ],
+    },
+    {
+      name: 'Laura',
+      roles: ['Lucy', 'Ensemble', 'Ensemble', 'Ensemble'],
+    },
+    {
+      name: 'Leonhard',
+      roles: ['Gouverneur Campbell', 'Kaiser Heinrich', 'Wirt Otto', 'Richy'],
+    },
+    {
+      name: 'Marc',
+      roles: [null, null, null, null, 'Theodor', 'Wilhelm'],
+    },
+    {
+      name: 'Marleen',
+      roles: ['Kate Campbell', 'Prinzessin Elisabeth', 'Martha', 'Mira'],
+    },
+    {
+      name: 'Paul',
+      roles: [null, null, 'Ensemble', 'Ensemble', 'Ensemble'],
+    },
+    {
+      name: 'Paulina',
+      roles: [null, null, null, null, null, 'Ensemble'],
+    },
+    {
+      name: 'Till',
+      roles: [
+        null,
+        'Mattes',
+        'Hans',
+        'Python',
+        'Archibald',
+        'Barde Mattes',
+        'Eddie',
+      ],
+    },
+    {
+      name: 'Toni',
+      roles: ['Captain Anne', 'Agnes', 'Emma'],
+    },
+  ],
+  tech: [
+    {
+      name: 'Christian',
+      icon: 'camera',
+      job: 'Videotechnik',
+    },
+    {
+      name: 'Daniel',
+      icon: 'lightbulb',
+      job: 'Lichttechnik',
+    },
+    {
+      name: 'Florian',
+      icon: 'settings',
+      job: 'Bühnentechnik',
+    },
+    {
+      name: 'Leo',
+      icon: 'settings',
+      job: 'Bühnentechnik',
+    },
+    {
+      name: 'Noah',
+      icon: 'lightbulb',
+      job: 'Lichttechnik',
+    },
+    {
+      name: 'Wolfgang',
+      icon: 'headphones',
+      job: 'Tontechnik',
+    },
+  ],
+}
+
+const teamMembers = team.current.map((member) => {
+  return member.name
+})
+
+export default function Team() {
+  return (
+    <div className='h-screen w-screen rounded-md flex flex-col md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden'>
+      <ParallaxScroll images={teamMembers}></ParallaxScroll>
+    </div>
+  )
+}
+
+// Path: app/team/page.tsx
---
+++ components/ui/team-card.tsx
@@ -0,0 +1,119 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useRef, useEffect, useState } from 'react'
+
+type TeamCardProps = {
+  image: string
+}
+
+function TeamCard({ image }: TeamCardProps) {
+  const cardRef = useRef(null)
+  const [transform, setTransform] = useState('')
+  const [backgroundImage, setBackgroundImage] = useState('')
+  const [transitionDuration, setTransitionDuration] = useState('300ms')
+  const [boxShadow, setBoxShadow] = useState('0 1px 5px #00000099')
+  const [imageSrc, setImageSrc] = useState(`/img/team/avatar/${image}.jpg`)
+
+  useEffect(() => {
+    const card = cardRef.current
+    const bounds = card.getBoundingClientRect()
+
+    function rotateToMouse(e: { offsetX: any; offsetY: any }) {
+      const leftX = e.offsetX
+      const topY = e.offsetY
+      const center = {
+        x: leftX - bounds.width / 2,
+        y: topY - bounds.height / 2,
+      }
+      const distance = Math.sqrt(center.x ** 2 + center.y ** 2)
+
+      setTransform(`
+        scale3d(1.07, 1.07, 1.07)
+        rotate3d(
+          ${center.y / 100},
+          ${-center.x / 100},
+          0,
+          ${Math.log(distance) * 4}deg
+        )
+      `)
+
+      setBackgroundImage(`
+        radial-gradient(
+          circle at
+          ${center.x * 2 + bounds.width / 2}px
+          ${center.y * 2 + bounds.height / 2}px,
+          #ffffff55,
+          #0000000f
+        )
+      `)
+    }
+
+    card.addEventListener('mousemove', rotateToMouse)
+    card.addEventListener('mouseleave', () => {
+      setTransform('')
+      setBackgroundImage('')
+    })
+
+    return () => {
+      card.removeEventListener('mousemove', rotateToMouse)
+    }
+  }, [])
+
+  return (
+    <div
+      ref={cardRef}
+      className='card aspect-square cursor-pointer rounded-lg'
+      style={{
+        fontWeight: 'bold',
+        boxShadow,
+        position: 'relative',
+        transitionDuration,
+        transitionProperty: 'transform, box-shadow',
+        transitionTimingFunction: 'ease-out',
+        transform: transform,
+      }}
+      onMouseOver={() => {
+        setTransitionDuration('150ms')
+        setBoxShadow('0 5px 20px 5px #00000044')
+      }}
+      onMouseOut={() => {
+        setTransitionDuration('300ms')
+        setBoxShadow('0 1px 5px #00000099')
+      }}
+      onClick={
+        // Navigate to the team member page
+        () => {
+          console.log('Navigate to the team member page')
+        }
+      }
+    >
+      <Image
+        src={imageSrc}
+        alt='Team member'
+        className='rounded-lg object-cover'
+        sizes='100% 100%'
+        fill
+        onError={(e) => {
+          console.log('Error loading image', e)
+          setImageSrc('/img/team/avatar/Placeholder.svg')
+        }}
+      />
+      <div className='absolute flex bg-black bg-opacity-50 text-white m-2 p-2 rounded-lg justify-end items-top right-0'>
+        {image}
+      </div>
+      <div
+        className='glow rounded-lg'
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          left: '0',
+          top: '0',
+          backgroundImage: backgroundImage,
+        }}
+      />
+    </div>
+  )
+}
+
+export default TeamCard
